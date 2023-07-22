4bbcf38556f4aba81a0e1397be27fab3a78170e4
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-07-22 17:50:25 +0530

    04built-in-middleware-for-post-request-body end

---
+++ 03mycontact-backend-contactController-created/controllers/contactController.js
@@ -0,0 +1,42 @@
+// @desc Get all contacts
+// @route GET /api/contacts
+// @access public
+
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "get all contacts" });
+};
+
+// @desc create all contact
+// @route POST /api/contacts
+// @access public
+
+const createContact = (req, res) => {
+  res.status(201).json({ message: "Create Contact" });
+};
+
+// @desc Get single contact
+// @route GET /api/contacts/:id
+// @access public
+
+
+const getContact = (req, res) => {
+    res.status(200).json({ message: `get contact for ${req.params.id}` });
+  }
+// @desc update contacts
+// @route PUT /api/contacts
+// @access public
+
+const updateContact = (req, res) => {
+    res.status(200).json({ message: `update contact for ${req.params.id}` });
+  }
+
+// @desc create all contacts
+// @route DELETE /api/contacts:id
+// @access public
+
+const deleteContact = (req, res) => {
+    res.status(200).json({ message: `delete contact for ${req.params.id}` });
+  }
+
+
+module.exports = {getContacts, getContact, createContact,updateContact,deleteContact };

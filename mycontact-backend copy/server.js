e7d3d6a80e464725585074811c5872cc97d0d7f3
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-07-22 09:23:19 +0530

    server created

--- mycontact-backend copy/server.js
+++ mycontact-backend copy/server.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.get("/api/contacts",(req,res)=>{
     res.send("Get all contacts");
 })
-
-const port = process.env.PORT || 5000;
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)

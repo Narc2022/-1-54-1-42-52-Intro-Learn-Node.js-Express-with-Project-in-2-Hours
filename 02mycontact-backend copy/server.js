300aed036f1d03560157a84f980fd61f00eedc7f
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-07-22 13:27:42 +0530

    02mycontact-backend-created apies-routes

--- 02mycontact-backend copy/server.js
+++ 02mycontact-backend copy/server.js
@@ -3,9 +3,7 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/api/contacts",(req,res)=>{
-    res.send("Get all contacts");
-})
+app.use("/api/contacts",require("./routes/contactRouters"));
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)

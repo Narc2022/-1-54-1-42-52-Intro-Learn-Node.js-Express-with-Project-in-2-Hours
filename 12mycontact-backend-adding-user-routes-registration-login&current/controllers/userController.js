931e7c714041975563208beb2bb1ffea9601bcf3
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-07-25 08:22:43 +0530

    get currentUser

--- 12mycontact-backend-adding-user-routes-registration-login&current/controllers/userController.js
+++ 12mycontact-backend-adding-user-routes-registration-login&current/controllers/userController.js
@@ -71,7 +71,7 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current User Information" });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
